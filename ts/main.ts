dc98464155b3db914b22fe16ff427d650d85775c
Vladyslav Bumbu <bumbuv136@gmail.com>
2023-10-18 17:23:37 -0700

    Documented Book class

--- ts/main.ts
+++ ts/main.ts
@@ -2,9 +2,25 @@
  * Represents a individual book that can be purchased
  */
 class Book {
+    /**
+     * The 13 digits ISBN number
+     */
     isbn : string;
+
+    /**
+     * This is the title of the book
+     */
     title : string;
+
+    /**
+     * The retail price of the book
+     */
     price : number;
+
+    /**
+     * The date the book was first published.
+     * This could be the future date, if the book is not yet released.
+     */
     releaseDate : Date;
 
 }
